55dd8e005d16c03f2347e24fc0f8a5c0a4a066ef
shake9 <shake7366@gmail.com>
2022-11-11 13:28:47 +0900

    弾の制御できた(ほんと)

--- Graduation_work/Assets/Scenes/boneTest.cs
+++ Graduation_work/Assets/Scenes/boneTest.cs
@@ -14,7 +14,7 @@ public class boneTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     [SerializeField]
