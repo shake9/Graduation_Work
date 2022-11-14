aa74bce8d64029345398aa7035fb2939aedc8a73
shake9 <shake7366@gmail.com>
2022-11-14 10:39:49 +0900

    的とスポナー追加

---
+++ Graduation_work/Assets/Scenes/HitController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitController : MonoBehaviour
+{
+    [SerializeField]
+    private string name;
+    private float Hp;
+    private Animator animator;
+
+    private void Awake()
+    {
+        Hp = 1.0f;
+        animator = GetComponent<Animator>();
+
+    }
+    //当たり判定メソッド
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    //衝突したオブジェクトがBullet(大砲の弾)だったとき
+    //    if (collision.gameObject.CompareTag("box"))
+    //    {
+    //        Debug.Log("敵と弾が衝突しました！！！");
+    //        Destroy(gameObject);
+
+    //    }
+    //}
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.gameObject.CompareTag(name))
+        {
+            Debug.Log("敵と弾が衝突しました！！！");
+
+            Hp -= 0.25f;
+
+        }
+    }
+    private void Update()
+    {
+        if (Hp <= 0.0f)
+        {
+            Destroy(gameObject);
+        }
+        animator.SetFloat("hp", Hp);
+    }
+}
