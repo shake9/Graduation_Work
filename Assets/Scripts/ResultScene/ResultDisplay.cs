8d66c3bcabd5ee346f664973310a7f960d61ea1b
鶴見昌之 <tsuruwatcher@gmail.com>
2023-01-23 16:05:15 +0900

    リザルトを昔の方に戻した

--- Assets/Scripts/ResultScene/ResultDisplay.cs
+++ Assets/Scripts/ResultScene/ResultDisplay.cs
@@ -1,27 +1,24 @@
-using System.Collections;
+ï»¿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ResultDisplay : MonoBehaviour
 {
-    // ƒXƒRƒA•\¦ƒeƒLƒXƒg
+    // ã‚¹ã‚³ã‚¢è¡¨ç¤ºãƒ†ã‚­ã‚¹ãƒˆ
     [SerializeField] private Text scoreNumText;
 
-    // ƒ}ƒCƒiƒX
+    // ãƒã‚¤ãƒŠã‚¹
     [SerializeField] private Image minus;
 
-    // ”íƒ_ƒ[ƒW”•\¦ƒeƒLƒXƒg
+    // è¢«ãƒ€ãƒ¡ãƒ¼ã‚¸æ•°è¡¨ç¤ºãƒ†ã‚­ã‚¹ãƒˆ
     [SerializeField] private Text damageTakenText;
 
-    // ÅIƒXƒRƒAƒeƒLƒXƒg
+    // æœ€çµ‚ã‚¹ã‚³ã‚¢ãƒ†ã‚­ã‚¹ãƒˆ
     [SerializeField] private Text totalScoreText;
 
     private void Start()
     {
-        ScoreManager.Instance.AddEnemyKillScore(100000, ScoreManager.ScoreType.Normal);
-        ScoreManager.Instance.AddPlayerDamageCount(11);
-
         StartCoroutine(ResultCoroutine());
     }
 
@@ -31,35 +28,35 @@ public class ResultDisplay : MonoBehaviour
         int damageCount = ScoreManager.Instance.DamageCount;
         int damageScore = 10 * damageCount;
 
-        // ƒXƒRƒA•\¦
+        // ã‚¹ã‚³ã‚¢è¡¨ç¤º
         scoreNumText.text = score.ToString();
         scoreNumText.gameObject.SetActive(true);
 
-        // ‘Ò‹@
+        // å¾…æ©Ÿ
         yield return new WaitForSeconds(0.5f);
 
         minus.gameObject.SetActive(true);
 
-        // ‘Ò‹@
+        // å¾…æ©Ÿ
         yield return new WaitForSeconds(0.5f);
 
-        // ”í’e”•\¦
+        // è¢«å¼¾æ•°è¡¨ç¤º
         damageTakenText.text = "10 x " + damageCount.ToString() + " Hit = " + damageScore.ToString();
         damageTakenText.gameObject.SetActive(true);
 
-        // ‘Ò‹@
+        // å¾…æ©Ÿ
         yield return new WaitForSeconds(0.5f);
 
-        // ÅIƒXƒRƒA
+        // æœ€çµ‚ã‚¹ã‚³ã‚¢
         int totalScore = score - damageScore;
 
-        // Œ…”
+        // æ¡æ•°
         int level = totalScore.ToString().Length;
 
-        // ÅIƒXƒRƒA•\¦
+        // æœ€çµ‚ã‚¹ã‚³ã‚¢è¡¨ç¤º
         totalScoreText.gameObject.SetActive(true);
 
-        // 1Œ…‚¸‚Â•\¦
+        // 1æ¡ãšã¤è¡¨ç¤º
         for (int i = 0; i < level + 1; i++)
         {
             totalScoreText.text = totalScore.ToString().Substring(level - i, i);
